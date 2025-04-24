8f17dfa76f4c2df6851872e6ee01a19ee0aa11ba
tobiasonandia <tobiasonandia0@gmail.com>
2025-04-23 22:41:26 -0300

    refactor: better semantic in Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,18 +2,15 @@ export const Hero = () => {
   return (
     <section className="py-24">
       {/* Headline principal */}
-      <h2 className="text-5xl font-bold leading-tight mb-6">
+      <h2 className="text-5xl border-b-5 pb-8 w-fit border-gray-300 font-bold leading-tight mb-6">
         Comparte tus mejores ideas
         <span className="block mt-2 text-3xl font-normal italic">
           Reflexiones para mentes curiosas
         </span>
       </h2>
 
-      {/* Divider decorativo */}
-      <div className="my-8 h-1 bg-gray-300 w-1/4" />
-
       {/* Cuerpo de texto */}
-      <p className="text-lg text-gray-700 leading-relaxed text-justify ">
+      <p className="text-lg text-balance text-gray-700 leading-relaxed">
         En un mundo saturado de información, las verdaderas ideas persisten.
         Este espacio está dedicado a aquellos pensamientos que merecen
         trascender lo efímero.
